87e99555f44be59b5a8bd24a54df0815caa84bb5
Waiman Yau <wyau121@gmail.com>
2024-01-06 20:32:30 +0000

    Logic and labels showing correctly. Balance system works now.

--- meridians.js
+++ meridians.js
@@ -1,5 +1,22 @@
+function showTreatingMeridian(sickMeridian) {
+  printSickMeridian(treatingMeridians[sickMeridian].name);
+
+  document.querySelector('.js-s1').innerHTML = `System 1: ${treatingMeridians[sickMeridian].s1}`;
+  document.querySelector('.js-s2').innerHTML = `System 2: ${treatingMeridians[sickMeridian].s2}`;
+  document.querySelector('.js-s3').innerHTML = `System 3: ${treatingMeridians[sickMeridian].s3}`;
+  document.querySelector('.js-s4').innerHTML = `System 4: ${treatingMeridians[sickMeridian].s4}`;
+  document.querySelector('.js-s5').innerHTML = `System 5: ${treatingMeridians[sickMeridian].s5}`;
+}
+
+function printSickMeridian(sickMeridian) {
+  document.querySelector('.js-sick-meridian').innerHTML = `Sick Meridian: ${sickMeridian}`;
+}
+
+
+
 const treatingMeridians = {
   lung: {
+    name: 'Lung',
     s1: 'Spleen',
     s2: 'Bladder',
     s3: 'Large Intestine',
@@ -7,6 +24,7 @@ const treatingMeridians = {
     s5: 'Liver'
   },
   largeIntestine: {
+    name: 'Large Intestine',
     s1: 'Stomach',
     s2: 'Liver',
     s3: 'Lung',
@@ -14,6 +32,7 @@ const treatingMeridians = {
     s5: 'Stomach'
   },
   spleen: {
+    name: 'Spleen',
     s1: 'Lung',
     s2: 'Small Intestine',
     s3: 'Stomach',
@@ -21,6 +40,7 @@ const treatingMeridians = {
     s5: 'Heart'
   },
   stomach: {
+    name: 'Stomach',
     s1: 'Large Intestine',
     s2: 'Pericardium',
     s3: 'Spleen',
@@ -28,6 +48,7 @@ const treatingMeridians = {
     s5: 'Large Intestine'
   },
   heart: {
+    name: 'Heart',
     s1: 'Kidney',
     s2: 'Gallbladder',
     s3: 'Small Intestine',
@@ -35,6 +56,7 @@ const treatingMeridians = {
     s5: 'Spleen'
   },
   smallIntestine: {
+    name: 'Small Intestine',
     s1: 'Bladder',
     s2: 'Spleen',
     s3: 'Heart',
@@ -42,6 +64,7 @@ const treatingMeridians = {
     s5: 'Bladder'
   },
   kidney: {
+    name: 'Kidney',
     s1: 'Heart',
     s2: 'Triple Burner',
     s3: 'Bladder',
@@ -49,6 +72,7 @@ const treatingMeridians = {
     s5: 'Pericardium'
   },
   bladder: {
+    name: 'Bladder',
     s1: 'Small Intestine',
     s2: 'Lung',
     s3: 'Kidney',
@@ -56,6 +80,7 @@ const treatingMeridians = {
     s5: 'Small Intestine'
   },
   pericardium: {
+    name: 'Pericardium',
     s1: 'Liver',
     s2: 'Stomach',
     s3: 'Triple Burner',
@@ -63,6 +88,7 @@ const treatingMeridians = {
     s5: 'Kidney'
   },
   tripleBurner: {
+    name: 'Triple Burner',
     s1: 'Gallbladder',
     s2: 'Kidney',
     s3: 'Pericardium',
@@ -70,6 +96,7 @@ const treatingMeridians = {
     s5: 'Gallbladder'
   },
   liver: {
+    name: 'Liver',
     s1: 'Pericardium',
     s2: 'Large Intestine',
     s3: 'Gallbladder',
@@ -77,18 +104,11 @@ const treatingMeridians = {
     s5: 'Lung'
   },
   gallbladder: {
+    name: 'Gallbladder',
     s1: 'Triple Burner',
     s2: 'Heart',
     s3: 'Liver',
     s4: 'Heart',
     s5: 'Triple Burner'
   }
-}
-
-function showTreatingMeridian(sickMeridian) {
-  document.querySelector('.js-s1').innerHTML = `System 1: ${treatingMeridians[sickMeridian].s1}`;
-  document.querySelector('.js-s2').innerHTML = `System 2: ${treatingMeridians[sickMeridian].s2}`;
-  document.querySelector('.js-s3').innerHTML = `System 3: ${treatingMeridians[sickMeridian].s3}`;
-  document.querySelector('.js-s4').innerHTML = `System 4: ${treatingMeridians[sickMeridian].s4}`;
-  document.querySelector('.js-s5').innerHTML = `System 5: ${treatingMeridians[sickMeridian].s5}`;
 }
